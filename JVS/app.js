72e536f97bfa371f2fc199c8377a2ffa08170ac7
Eudald Ferrer <eudald.ferrer@gmail.com>
2022-04-25 12:47:30 +0200

    Array check

--- JVS/app.js
+++ JVS/app.js
@@ -23,31 +23,86 @@ const mockData = [
         'ingredient': 'Chicken',
         'path': '/pizza/IMAGES/pizza4.png'
     }, {
-        'name': 'California pizza',
+        'name': 'Calfornia pizza',
         'price': '$ 8.50',
         'ingredient': 'Mushroom',
         'path': '/pizza/IMAGES/pizza1.png'
     },
     {
-        'name': 'Greek pizza',
+        'name': 'Grek pizza',
         'price': '$ 12.99',
         'ingredient': 'Beef',
         'path': '/pizza/IMAGES/pizza3.png'
     },
     {
-        'name': 'Sicilian pizza',
+        'name': 'Siclian pizza',
         'price': '$ 7.90',
         'ingredient': 'Tomato',
         'path': '/pizza/IMAGES/pizza2.png'
     },
     {
-        'name': 'Louis pizza',
+        'name': 'Lois pizza',
         'price': '$ 4.79',
         'ingredient': 'Chicken',
         'path': '/pizza/IMAGES/pizza4.png'
     }
 ]
 
+document.addEventListener('DOMContentLoaded', () => {
+    mockData.forEach(pizzaObj => {
+        let pizzaName = pizzaObj['name'];
+        let pizzaIngredient = pizzaObj['ingredient']
+        let pizzaPath = pizzaObj['path']
+        let pizzaPrice = pizzaObj['price']
+        print_card(pizzaName, pizzaIngredient, pizzaPath, pizzaPrice)
+    })
+})
+
+function prompt_pizza(){
+    let pizzArray = []
+    let pizzaPrice
+    let pizzaName
+    mockData.forEach(pizzaObj =>{
+    pizzaPrice = pizzaObj['price']
+    pizzaName = pizzaObj['name']
+    })
+    var pizza_cantidad = prompt('¿Cuantas unidades quieres de la pizza?', 'RECORDAD QUE SOLO SE ACEPTAN VALORES NUMERICOS');
+    var pizza = pizza + pizza_cantidad
+    var number = new Boolean(isNaN(pizza_cantidad))
+    if(number == false){
+        window.alert(`Se han añadido ${pizza_cantidad} pizzas al carrito de la compra / El total de las ${pizza_cantidad} es: ${pizzaPrice}€`);
+        if(pizzaName === 'California pizza'){
+            pizzArray.splice(0, 0, pizza_cantidad)
+            console.log(pizzArray[0])
+        }if(pizzaName === 'Greek pizza'){
+            pizzArray.splice(1, 0, pizza_cantidad)
+            console.log(pizzArray[1])
+        }if(pizzaName === 'Sicilian pizza'){
+            pizzArray.splice(2, 0, pizza_cantidad)
+            console.log(pizzArray[2])
+        }if(pizzaName === 'Louis pizza'){
+            pizzArray.splice(3, 0, pizza_cantidad)
+            console.log(pizzArray[3])
+        }if(pizzaName === 'Calfornia pizza'){
+            pizzArray.splice(4, 0, pizza_cantidad)
+            console.log(pizzArray[4])
+        }if(pizzaName === 'Grek pizza'){
+            pizzArray.splice(5, 0, pizza_cantidad)
+            console.log(pizzArray[5])
+        }if(pizzaName === 'Siclian pizza'){
+            pizzArray.splice(6, 0, pizza_cantidad)
+            console.log(pizzArray[6])
+        }if(pizzaName === 'Lois pizza'){
+            pizzArray.splice(7, 0, pizza_cantidad)
+            console.log(pizzArray[7])
+        }
+        console.log(pizzArray)
+    }if(number == true){
+        window.alert("El valor indicado no es numérico");
+    }
+
+}
+// FUNCION QUE CREA LAS CARTAS DE LAS PIZZAS
 function print_card(name, ingredient, path, price){
     const cardContainer = document.getElementById('container_card');
     let card = `<div class="card_box">
@@ -61,7 +116,7 @@ function print_card(name, ingredient, path, price){
                         </div>
                         <div class="card_item_row">
                             <p class="price_text">${price}</p>
-                            <button onclick="" class="pizza_button">+</button>
+                            <button class="pizza_button" onclick="prompt_pizza()">+</button>
                         </div>
                     </div>
                 </div>`
@@ -70,28 +125,13 @@ function print_card(name, ingredient, path, price){
 
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    mockData.forEach(pizzaObj => {
-        let pizzaName = pizzaObj['name'];
-        let pizzaIngredient = pizzaObj['ingredient']
-        let pizzaPath = pizzaObj['path']
-        let pizzaPrice = pizzaObj['price']
-        print_card(pizzaName, pizzaIngredient, pizzaPath, pizzaPrice)
-    })
-})
 
-function ask_pizza(pizzaPrice){
-    console.log("hola")
-    var pizza_cantidad = prompt('¿Cuantas unidades quieres de la pizza?', 'RECORDAD QUE SOLO SE ACEPTAN VALORES NUMERICOS');
-    var precio_pizza = (pizzaPrice)*pizza_cantidad
-    var pizza = pizza + pizza_cantidad
-    var number = new Boolean(isNaN(pizza_cantidad))
-    if(number == false){
-        window.alert("Se han añadido "+pizza_cantidad+" pizzas "+`${pizzaName}`+" al carrito de la compra"+"/ El total de las "+pizza_cantidad+" es: "+precio_pizza+"€");
-    }if(number == true){
-        window.alert("El valor indicado no es numérico");
-    }
+// FUNCION PARA PEDIR CUANTAS UNIDADES DE PRODUCTO QUIERE EL CLIENTE
 
-}
-
-document.getElementByClass("pizza_button").addEventListener('click', ask_pizza(pizzaPrice));
+// EVENTO ONCLICK PARA VINCULAR LA FUNCION DEL PROMPT AL BOTÓN
+// const btnPizza = document.getElementById('btnAumentar')
+// btnPizza.addEventListener('click',function ()
+//     {
+//      alert("hello");
+//      //validation code to see State field is mandatory.  
+//     }  );
